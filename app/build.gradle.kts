b8c9d562c7fbc3473d37f59c2c5540bded3ee076
adirifta <adiriftadk@gmail.com>
2023-12-08 21:47:14 +0700

    grafik

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -15,6 +16,10 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildFeatures {
+        viewBinding = true
     }
 
     buildTypes {
@@ -37,11 +42,15 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-database:20.3.0")
+    implementation("com.google.android.gms:play-services-fitness:21.1.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
+    implementation ("com.jjoe64:graphview:4.2.2")
 }
--- app/src/main/java/com/example/aplikasiagriculture/MainActivity.kt
+++ app/src/main/java/com/example/aplikasiagriculture/MainActivity.kt
@@ -1,11 +1,91 @@
 package com.example.aplikasiagriculture
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.*
+import com.jjoe64.graphview.DefaultLabelFormatter
+import com.jjoe64.graphview.GraphView
+import com.jjoe64.graphview.series.DataPoint
+import com.jjoe64.graphview.series.LineGraphSeries
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var graphView: GraphView
+    private lateinit var series: LineGraphSeries<DataPoint>
+    private lateinit var database: FirebaseDatabase
+    private lateinit var reference: DatabaseReference
+
+    private val sdf = SimpleDateFormat("hh:mm:ss", Locale.getDefault())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        graphView = findViewById(R.id.graphView)
+        series = LineGraphSeries()
+        graphView.addSeries(series)
+
+        database = FirebaseDatabase.getInstance()
+        reference = database.getReference("Tubes")
+
+        setupGraphView()
+
+        // You can remove setListeners() since the button and text input are removed
+    }
+
+    private fun setupGraphView() {
+        graphView.gridLabelRenderer.numHorizontalLabels = 3
+
+        graphView.gridLabelRenderer.labelFormatter = object : DefaultLabelFormatter() {
+            override fun formatLabel(value: Double, isValueX: Boolean): String {
+                return if (isValueX) {
+                    sdf.format(Date(value.toLong()))
+                } else {
+                    super.formatLabel(value, isValueX)
+                }
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        reference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val hourlyData = mutableMapOf<Int, MutableList<Double>>()
+
+                for (myDataSnapshot in dataSnapshot.children) {
+                    val timestamp = myDataSnapshot.child("xValue").getValue(Long::class.java)
+                    val value = myDataSnapshot.child("yValue").getValue(Double::class.java)
+
+                    if (timestamp != null && value != null) {
+                        // Extract the hour from the timestamp
+                        val hour = Calendar.getInstance().apply {
+                            timeInMillis = timestamp
+                        }.get(Calendar.HOUR_OF_DAY)
+
+                        // Add the value to the corresponding hour
+                        if (hourlyData.containsKey(hour)) {
+                            hourlyData[hour]?.add(value)
+                        } else {
+                            hourlyData[hour] = mutableListOf(value)
+                        }
+                    }
+                }
+
+                val averageHourlyData = hourlyData.map { (hour, values) ->
+                    val average = values.average()
+                    DataPoint(hour.toDouble(), average)
+                }.toTypedArray()
+
+                series.resetData(averageHourlyData)
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Handle errors here
+            }
+        })
     }
 }
